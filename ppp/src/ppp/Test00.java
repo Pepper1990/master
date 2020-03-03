ea9f939bc578d8fc0a633d2baeb8fd1905df5bba
Pepper1990 <rt1937@outlook.com>
2020-03-03 22:41:26 +0800

    first commit11111

--- ppp/src/ppp/Test00.java
+++
@@ -1,7 +0,0 @@
-package ppp;
-
-public class Test00 {
-	public static void main(String[] args) {
-		System.out.println("第0次修改");
-	}
-}
---
+++ ttt00/src/ttt00/TT.java
@@ -0,0 +1,7 @@
+package ttt00;
+
+public class TT {
+	public static void main(String[] args) {
+		System.out.println("helloworld");
+	}
+}
